4b69e3e99260637f78b92dad5df6228800da93af
Shruti Damkondwar <shrutidamkondwar6@gmail.com>
2025-09-28 02:10:13 +0530

    Delete Celsius to Fahrenheit.c

--- Celsius to Fahrenheit.c
+++
@@ -1,9 +0,0 @@
-//Write a C program to convert temperature from Celsius to Fahrenheit using the
-//formula:
-//F = (C *9/5) + 32
-void main()
-{
-   double	Celsius=85,Fahrenheit;
-    Fahrenheit= (Celsius *9/5) + 32;
-    printf("Fahrenheit %lf",Fahrenheit);
-}
