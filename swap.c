ac196d1f41728495297fcf6483e4f9c38a743e85
Shruti Damkondwar <shrutidamkondwar6@gmail.com>
2025-09-28 02:21:39 +0530

    Delete swap.c

--- swap.c
+++
@@ -1,9 +0,0 @@
-//Write a C program to swap two numbers using a temporary third variable.
-void main()
-{
-	int a=10,b=20,temporary;
-	temporary=a;
-	a=b;
-	b=temporary;
-	printf("swap two number=a=%d b=%d ",a,b);
-}
