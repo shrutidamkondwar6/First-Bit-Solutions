eb686cda1eab2af2352be09d383bf8de378042fa
Shruti Damkondwar <shrutidamkondwar6@gmail.com>
2025-09-28 02:12:19 +0530

    Delete area of triangle.c

--- area of triangle.c
+++
@@ -1,8 +0,0 @@
-//Write a C program to input the base and height of a triangle and calculate its area.
-void main()
-{
-	float base=10,height=20,area;
-	area=0.5*base*height;
-	printf("area=%f",area);
-	
-}
