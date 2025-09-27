d7b656aacddb33b60c4cd49e70b8de047cc49298
Shruti Damkondwar <shrutidamkondwar6@gmail.com>
2025-09-28 02:20:37 +0530

    Delete square cube.c

--- square cube.c
+++
@@ -1,9 +0,0 @@
-//Write a C program to find the square and cube of a given number.
-void main()
-{
-	int square=10,cube=20;
-	square=square*square;
-	cube=cube*cube*cube;
-	printf("square of no= %d",square);
-	printf("cube of no= %d",cube);
-}
